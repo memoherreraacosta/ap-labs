8b1091505d7b04a2dc1da9038fd383d96a1119f9
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-02-01 10:10:09 -0600

    Mejores lab0.2

--- labs/lab0.2/str-main.c
+++ labs/lab0.2/str-main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv);{
-	printf("Initial length : %d\n",strlen(argv[1]));
-	char* ns = stradd(argv[1],argv[2]);
+int main(int argc, char **argv){
+
+	if(argc == 4){
+	//1
+		printf("Initial length : %d\n",strlen(argv[1]));
+
+	//2
+		char *ns = stradd(argv[1],argv[2]);	
+		printf("New String : %s\n",ns);
 	
-	printf("New String : %s\n",ns);
-	
-	char awr = 'no';
-	if(strfind(ns, argv[3]) == 0){
-		awr = 'yes';
+	//3
+		char awr = 'no';
+		if(strfind(ns, argv[3]) == 0){
+			awr = 'yes';
+		}
+		printf("Substring was found : %c",awr);
+
+	}else {
+		printf("Invalid number of arguments");
 	}
-	printf("Substring was found : %c",awr);
-	
 }
--- labs/lab0.2/strlib.c
+++ labs/lab0.2/strlib.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 int strlen(char *str){
 	int x = 0;
 	for(int i = 0; str[i] != '\0' ; i++){
@@ -8,14 +6,15 @@ int strlen(char *str){
 	return x;
 }
 
-char* stradd(char *origin, char* addition){
-	char* newS;
+char* stradd(char *origin, char *addition){
+	char *newS;
 
-	newS = (char* )malloc(strlen(strlen(origin)+strlen(addition)+1));
+	newS = (char *)malloc(strlen(origin)+strlen(addition)+1);
 	if(!newS){
-		perror("Failed to allocate space because ");
+		perror("Failed to allocate space because: ");
 		exit(1);
 	}
+
 	for(int i = 0 ; i < len(origin) ; i++){
 		newS[i] = origin[i];
 	}
@@ -24,16 +23,19 @@ char* stradd(char *origin, char* addition){
 		newS[i] = addition[x];
 		x += 1; 
 	}
+	newS[x] = "\0";
 	return newS;
 }
 
-int strfind(char *origin, char* substr){
+int strfind(char *origin, char *substr){
 	int found = 1; //True = 0 , False = 1
+	int flag;
+
 	for(int i = 0; i < len(origin) ; i++){
 		if(origin[i] == substr[0]){
 			//Se entrara a un ciclo donde se buscara comprobar que la palabra substr esta dentro de origin
-			int flag = len(substr) - 1;
-			for(int j = 1 ; j < len(substr) && (j+i) < len(origin) ; j ++){
+			flag = len(substr);
+			for(int j = 0 ; j < len(substr) && (j+i) < len(origin) ; j ++){
 				if (substr[j] == origin[j+i]){
 					flag -= 1;
 				}

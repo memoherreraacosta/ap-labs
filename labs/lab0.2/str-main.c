ed4191e5b0fea5a9cb00f68e161e6b915d5726a1
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-02-05 14:38:27 -0600

    Linking in C complete

--- labs/lab0.2/str-main.c
+++ labs/lab0.2/str-main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+int strlen(char *str);
+
+char* stradd(char *origin, char *addition);
+
+int strfind(char *origin, char *substr);
+
+
 int main(int argc, char **argv){
 
 	if(argc == 4){
 	//1
-		printf("Initial length : %lu\n",strlen(argv[1]));
+		printf("Initial length : %d\n",strlen(argv[1]));
 
 	//2
 		char *ns = stradd(argv[1],argv[2]);	

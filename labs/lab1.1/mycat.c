a7aa90e6eaa224caab9168688e4dd017a0a4802e
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-02-08 18:50:28 -0600

    Mejora archivo

--- labs/lab1.1/mycat.c
+++ labs/lab1.1/mycat.c
@@ -10,16 +10,18 @@ int main(int argc, char **argv){
 	FILE* archivito;
 	
 	if((archivito = fopen(argv[1], "r")) != NULL){
-		int c, index = 0;
-		char textito[2048];
+		int c, index = -1, sizeS = 2048;
+		char textito[sizeS];
 
    		while ((c = getc(archivito)) != EOF)
        			textito[index++] = c;
-		
+
+		textito[index++] = '\0';
+		fclose(archivito);
 		printf("%s",textito);
 	}else{
 		printf("Error al leer el archivo\n");
 		return 2;
 	}
-	fclose(archivito);
+	
 }

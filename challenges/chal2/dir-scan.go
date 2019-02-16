970052ef51392dd6991a5533d980cc9c82d84b53
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-02-15 23:59:42 -0600

    Go problem

--- challenges/chal2/dir-scan.go
+++ challenges/chal2/dir-scan.go
@@ -3,10 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"io/ioutil"
 )
 
 // scanDir stands for the directory scanning implementation
-func scanDir(dir string) error {
+func scanDir(dirPath string) error {
+
+	files , err := ioutil.ReadDir(dirPath)
+	total := len(dirFiles)
+	dir := 0
+	sLin := 0
+	otros := 0
+
+	for err, fil := range files {
+		if fil.Mode()&os.ModeDir != 0 {
+			dir++
+		} else if fil.Mode()&os.ModeSymlink != 0 {
+			sLin++
+		} else {
+			otros++
+		}
+	}
+	
+	fmt.Printf("+----------------+------+")
+	fmt.Printf("| Path:          |  %s  |", dirPath)
+	fmt.Printf("+----------------+------+")
+	fmt.Printf("| Directories    |  %d  |\n", dir))
+	fmt.Printf("| Symbolic Links |  %d  |\n", sLink)
+	fmt.Printf("| Other files    |  %d  |\n", other)
+	fmt.Printf("+----------------+------+")
+
 	return nil
 }
 

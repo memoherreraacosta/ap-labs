989c08dcc948989e2beddd0a59d81cbff5da81bd
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-02-25 19:58:38 -0600

    Actualizacion lab 2.3

--- labs/lab2.3/cross-ref.c
+++ labs/lab2.3/cross-ref.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#define REPORT_FILE "list.txt"
+#include <ctype.h>
+#include <string.h>
+#define REPORT_LIST "list.txt"
+#define REPORT_FILE "newFile.txt"
 
 int createReport(char *fRead, char *fList); 
-int omitir(char *test);
+int omitir(char* test);
+int omitChar(char c);
 
 int main(int argc, char **argv) {
 
@@ -10,11 +14,11 @@ int main(int argc, char **argv) {
 	printf("Error in number of arguments\n");
 	return 1;
     }
-    int res = createReport(argv[1], REPORT_FILE);
-    return res;
+    
+    return createReport(argv[1], REPORT_FILE);
 }
 
-int createReport(char *fRead, char *fList) {
+int createReport(char *fRead, char *fWrite) {
 	FILE *fp1, *fp2;
 	char ch;
 	int pos;
@@ -24,51 +28,92 @@ int createReport(char *fRead, char *fList) {
         	return -1;
     	}
 
-    	fp2 = fopen(fList, "w");  
+    	fp2 = fopen(fWrite, "w");  
     	fseek(fp1, 0L, SEEK_END); // file pointer at end of file
     	pos = ftell(fp1);
     	fseek(fp1, 0L, SEEK_SET); // file pointer set at start
 
 	int lineC = 1;
-	
+	char cLow;
+	char palabra[45];
+
 	while (pos--){
         	ch = fgetc(fp1);  // copying file character by character
-        	
+
+		if(omitChar(ch) == -1){
+			//Char that wont be a word
+			fputc(ch,fp2);
+
+		}else if(ch == '\n'){
+			//New line 
+			fputc(ch,fp2);
+			lineC ++;
+
+		}else if(ch != '\n'){
+			//Is a word
 		
-		lineC ++;
-		fputc(ch, fp2);
+			for(int i = 0 ; ch != '\n' &&  omitChar(ch) != -1; i++){
+				palabra[i] = ch;
+				ch = fget(fp1);
+				cLow = tolower(ch);
+				pos--;
+			}
+
+			//Test palabra to put it into the file
+
+		}else{
+			printf("wtf : %c\n",cLow);
+		}
     	}
     	fclose(fp1);
     	fclose(fp2);	
 	return 0;
 }
-int omitir(char *test){
-	switch(test):
+int omitChar(char c){
+	switch(c){
+	case '.':	return -1;
+	case ' ':	return -1;
+	case ',':	return -1;
+	case ';':	return -1;
+	case ':':	return -1;
+	case '?':	return -1;
+	case '-':	return -1;
+	case '_':	return -1;
+	case '=':	return -1;
+	case '(':	return -1;
+	case ')':	return -1;
+	case '"':	return -1;
+	case '!':	return -1;
+	case '%':	return -1;
+	case '+':	return -1;
+	case '[':	return -1;
+	case ']':	return -1;
+	case '{':	return -1;
+	case '}':	return -1;
 	
-	case "and":
+	default:	return 0;
+	}
+}
+
+int omitir(char* test){
+	
+	if(strcmp("and",test) == 0 ||
+	   strcmp("the",test) == 0 ||
+	   strcmp("a",test) == 0 ||
+	   strcmp("to",test) == 0 ||
+	   strcmp("an",test) == 0 ||
+	   strcmp("of",test) == 0 ||
+           strcmp("for",test) == 0 ||
+	   strcmp("at",test) == 0 ||
+	   strcmp("from",test) == 0 ||
+	   strcmp("also",test) == 0 ||
+	   strcmp("in",test) == 0 ||
+	   strcmp("that",test) == 0)
+	{	
 		return -1;
-	case "the":
-		return -1;
-	case "a":
-		return -1;
-	case "to":
-		return -1;
-	case "an":
-		return -1;
-	case "of":
-		return -1;
-	case "for":
-		return -1;
-	case "at":
-		return -1;
-	case "from":
-		return -1;
-	case "also":
-		return -1;
-	case "in":
-		return -1;
-	case "that":
-		return -1;
-	default:
+	}
+	else
+	{
 		return 0;
+	}
 }

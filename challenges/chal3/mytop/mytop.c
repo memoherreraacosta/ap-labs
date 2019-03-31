0638b39afc826647223fbb6467a631228c537099
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-03-30 22:16:12 -0600

    Final correction, working

--- challenges/chal3/mytop/mytop.c
+++ challenges/chal3/mytop/mytop.c
@@ -131,131 +131,131 @@ void getTable(){
 		    strcat(fdpath, "/fd");
 
         fp = fopen(path, "r");
-        char buffer[255];
+        char buff[255];
 
             // Read file to get values 
 
-        while(fgets(buffer, 255, (FILE*) fp)) {
+        while(fgets(buff, 255, (FILE*) fp)) {
 
                 // Read file 
-          if(buffer[0]=='N'){
-            if(buffer[1]=='a' && buffer[2]=='m' && buffer[3]=='e'){
+          if(buff[0]=='N'){
+            if(buff[1]=='a' && buff[2]=='m' && buff[3]=='e'){
               int i;
               char nombre[20];
               int posN=0; 
-              int terminar=0;   
+              int flagDone =0;   
               for(i=5;i<255;i++){
-                if(buffer[i]=='\n'){
+                if(buff[i]=='\n'){
                   break;
                 }
-                if(buffer[i]==' '){
-                  if(terminar==1){
+                if(buff[i]==' '){
+                  if(flagDone ==1){
                     nombre[posN]='\0';
                       break;
                   }
                   continue;
                 }
 
-                nombre[posN]=buffer[i];
-                posN+=1;
-                terminar=1;
+                nombre[posN] = buff[i];
+                posN += 1;
+                flagDone = 1;
               }
               strcpy(processes[k].name,nombre);
               memset(nombre,0,20);
             }
-          }else if(buffer[0]=='S'){
-            if(buffer[1]=='t' && buffer[2]=='a' && buffer[3]=='t' && buffer[4]=='e'){
+          }else if(buff[0]=='S'){
+            if(buff[1]=='t' && buff[2]=='a' && buff[3]=='t' && buff[4]=='e'){
               int i;
-              char estado[20];
-              int posN=0;
-              int terminar=0;
+              char state[20];
+              int posN = 0;
+              int flagDone = 0;
 
               for(i=6;i<255;i++){
-                if(buffer[i]=='\n'){                   
+                if(buff[i]=='\n'){                   
                   break;
                 }
-                if(buffer[i]==' '){
-                  if(terminar==1){
-                    estado[posN]='\0';
+                if(buff[i]==' '){
+                  if(flagDone ==1){
+                    state[posN]='\0';
                     break;
                   }
                   continue;
                 }
-                estado[posN]=buffer[i];
+                state[posN]=buff[i];
                 posN+=1;
-                terminar=1;
+                flagDone =1;
               }
-              strcpy(processes[k].state,estado);
-              memset(estado,0,20);
+              strcpy(processes[k].state,state);
+              memset(state,0,20);
 
               }
-            }else if(buffer[0]=='P'){
-              if(buffer[1]=='i' && buffer[2]=='d'){
+            }else if(buff[0]=='P'){
+              if(buff[1]=='i' && buff[2]=='d'){
                 int i;
                 char dataPID[20];
                 int posN=0;
-                int terminar=0;
+                int flagDone =0;
                 for(i=4;i<255;i++){
-                  if(buffer[i]=='\n'){                       
+                  if(buff[i]=='\n'){                       
                     break;
                   }
-                  if(buffer[i]==' '){
-                    if(terminar==1){
+                  if(buff[i]==' '){
+                    if(flagDone ==1){
                       dataPID[posN]='\0';
                       break;
                     }
                     continue;
                   }
-                  dataPID[posN]=buffer[i];
+                  dataPID[posN]=buff[i];
                   posN+=1;
-                  terminar=1;
+                  flagDone =1;
                 }
                 strcpy(processes[k].pid,dataPID);
                 memset(dataPID,0,20);
 
-              }else if(buffer[1]=='P' && buffer[2]=='i' && buffer[3]=='d'){
+              }else if(buff[1]=='P' && buff[2]=='i' && buff[3]=='d'){
                 int i;
                 char dataPPID[20];
                 int posN=0;
-                int terminar=0;
+                int flagDone =0;
                 for(i=5;i<255;i++){
-                  if(buffer[i]=='\n'){
+                  if(buff[i]=='\n'){
                     break;
                   }
-                  if(buffer[i]==' '){
-                    if(terminar==1){
+                  if(buff[i]==' '){
+                    if(flagDone ==1){
                       dataPPID[posN]='\0';
                       break;
                     }
                     continue;
                   }
-                  dataPPID[posN]=buffer[i];
+                  dataPPID[posN]=buff[i];
                   posN+=1;
-                  terminar=1;
+                  flagDone =1;
                 }
                 strcpy(processes[k].ppid,dataPPID);
                 memset(dataPPID,0,20);
 
                }
-              }else if(buffer[0]=='V'){
+              }else if(buff[0]=='V'){
                   //VmRSS
-                if(buffer[1]=='m' && buffer[2]=='R' && buffer[3]=='S' && buffer[4]=='S'){
+                if(buff[1]=='m' && buff[2]=='R' && buff[3]=='S' && buff[4]=='S'){
 
                   int i;
                   char memoria[20];
                   int posN=0;
-                  int terminar=0;
+                  int flagDone =0;
                   for(i=6;i<255;i++){
-                    if(buffer[i]=='\n'){
+                    if(buff[i]=='\n'){
                       break;
                     }
-                    if(buffer[i]==' '){            
+                    if(buff[i]==' '){            
                       continue;
                     }
 
-                    memoria[posN]=buffer[i];
+                    memoria[posN]=buff[i];
                     posN+=1;
-                    terminar=1;
+                    flagDone =1;
                   }
                   if(memoria[0] == '\0'){
                     strcpy(processes[k].memory," ");
@@ -264,27 +264,27 @@ void getTable(){
                     memset(memoria,0,20);
                   }
                  }
-              }else if(buffer[0]=='T'){
+              }else if(buff[0]=='T'){
                   //Threads
-                if(buffer[1]=='h' && buffer[2]=='r' && buffer[3]=='e' && buffer[4]=='a' && buffer[5]=='d' && buffer[6]=='s'){
+                if(buff[1]=='h' && buff[2]=='r' && buff[3]=='e' && buff[4]=='a' && buff[5]=='d' && buff[6]=='s'){
                   int i;
                   char threads[20];
                   int posN=0;   
-                  int terminar=0;                 
+                  int flagDone =0;                 
                   for(i=8;i<255;i++){
-                    if(buffer[i]=='\n'){
+                    if(buff[i]=='\n'){
                       break;
                     }
-                    if(buffer[i]==' '){
-                      if(terminar==1){
+                    if(buff[i]==' '){
+                      if(flagDone ==1){
                         threads[posN]='\0';
                         break;
                       }
                       continue;
                     }
-                    threads[posN]=buffer[i];
+                    threads[posN]=buff[i];
                     posN+=1;
-                    terminar=1;
+                    flagDone =1;
                    }
                   if(threads[0] == '\0'){
                     strcpy(processes[k].memory," ");

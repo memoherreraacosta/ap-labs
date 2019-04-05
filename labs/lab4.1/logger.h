e5bf2ff08b39170f4cdc4c0644f6866a6ccc5605
Guillermo Herrera-Acosta <a01400835@itesm.mx>
2019-04-05 10:18:31 -0600

    loggers added

--- labs/lab4.1/logger.h
+++ labs/lab4.1/logger.h
@@ -1,7 +1,17 @@
-// Logger
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <execinfo.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
 
-int initLogger(char *logType);
+
+void textcolor(int attr, int fg, int bg);
 int infof(const char *format, ...);
 int warnf(const char *format, ...);
 int errorf(const char *format, ...);
 int panicf(const char *format, ...);
+void printBacktrace();
+void printBacktraceSyslog();
+int initLogger(char *logType);
